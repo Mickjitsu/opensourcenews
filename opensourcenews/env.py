0ba473828e30180e6eed50778baadb4ce9d52206
Michael Mc Court <mmccourt928@outlook.com>
2024-12-10 10:31:17 +0000

    due to error made with database, have removed old db and created a new one

--- opensourcenews/env.py
+++ opensourcenews/env.py
@@ -1,3 +1,3 @@
 import os
-os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:2qCiSLIA0PEp@ep-late-sun-a2upi3ho.eu-central-1.aws.neon.tech/trick_buck_blink_371300'
+os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:2qCiSLIA0PEp@ep-late-sun-a2upi3ho.eu-central-1.aws.neon.tech/value_boney_ashes_751026'
 os.environ['SECRET_KEY'] = '123mick1916'
