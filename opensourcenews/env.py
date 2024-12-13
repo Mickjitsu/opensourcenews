5c04a50fdc72dbb182e956792a19dd57533ecf16
Michael Mc Court <mmccourt928@outlook.com>
2024-12-13 12:17:43 +0000

    AWS Now able to load images held in bucket

--- opensourcenews/env.py
+++ opensourcenews/env.py
@@ -1,7 +1,7 @@
 import os
 os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:2qCiSLIA0PEp@ep-late-sun-a2upi3ho.eu-central-1.aws.neon.tech/value_boney_ashes_751026'
 os.environ['SECRET_KEY'] = '123mick1916'
-os.environ['AWS_ACCESS_KEY'] = 'AKIAQ3EGWB5IUYRLLKFI'
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAQ3EGWB5IUYRLLKFI'
 os.environ['AWS_S3_REGION_NAME'] = 'eu-north-1'
 os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
 os.environ['AWS_STORAGE_BUCKET_NAME'] = 'opensourcenewsmedia'
