d4286f919c59dbaad4e34aa137b8e5a24cb17c13
Michael Mc Court <mmccourt928@outlook.com>
2024-12-13 13:28:44 +0000

    404 page added

--- opensourcenews/articles/urls.py
+++ opensourcenews/articles/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.http import Http404
 from . import views
 
 
@@ -11,7 +12,8 @@ urlpatterns = [
      path('comment/edit/<int:comment_id>/', views.edit_comment, name='edit-comment'),
      path('comment/delete/<int:comment_id>/', views.delete_comment, name='delete-comment'),
      path('comment/<int:comment_id>/upvote/', views.upvote_comment, name='upvote_comment'),
-     path('comment/<int:comment_id>/downvote/', views.downvote_comment, name='downvote_comment')
+     path('comment/<int:comment_id>/downvote/', views.downvote_comment, name='downvote_comment'),
+     path('test-404/', lambda request: (_ for _ in ()).throw(Http404)),
 ]
 
 if settings.DEBUG:
