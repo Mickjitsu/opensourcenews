bcf3159904aeac1fab187d77000de33f19863d52
Michael Mc Court <mmccourt928@outlook.com>
2024-12-14 22:32:56 +0000

    bug resolution - changes require to image urls

--- opensourcenews/settings.py
+++ opensourcenews/settings.py
@@ -170,6 +170,7 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
+STATIC_MEDIA_URL = 'https://opensourcenewsmedia.s3.eu-north-1.amazonaws.com/'
 
 
 LOGIN_REDIRECT_URL = '/' 
